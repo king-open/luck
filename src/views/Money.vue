8a18ddcf570df7f8351d108b2dec88af49543a68
king-open <kingwang.0624@gmail.com>
2022-01-12 19:46:39 +0800

    Money 页面布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,107 @@
 <template>
-<div>
-  Money.vue
+<div class="nav-wrapper">
+  <div class="content">
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+    <p>Money.vue</p>
+  </div>
   <Nav/>
 </div>
 </template>
@@ -11,6 +112,16 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+.nav-wrapper{
+  border: 1px solid green;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+.content{
+  border: 1px solid blue;
+  overflow: auto;
+  flex-grow: 1;
+}
 </style>
