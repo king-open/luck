dbbd692f1f821f6787fe29922fae13d0ec218cdf
king-open <kingwang.0624@gmail.com>
2022-01-12 19:52:41 +0800

    重复三次布局

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    Labels.vue
+  <div class="nav-wrapper">
+    <div class="content">
+      Labels.vue
+    </div>
     <Nav/>
   </div>
 </template>
@@ -11,6 +13,16 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+.nav-wrapper{
+  border: 1px solid green;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+.content{
+  border: 1px solid blue;
+  overflow: auto;
+  flex-grow: 1;
+}
 </style>
